2f823e8cbeadaabf94478f69b5929bb0b77b4247
shubham <oulkarshubhu@gmail.com>
2025-02-14 11:41:00 +0530

    add linter for src folder

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,9 @@
-// @ts-check
-
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   eslint.configs.recommended,
-  tseslint.configs.recommendedTypeChecked,
+  tseslint.configs.strict,
   tseslint.configs.stylisticTypeChecked,
   {
     languageOptions: {
